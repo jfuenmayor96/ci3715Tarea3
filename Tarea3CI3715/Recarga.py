48dc3fc7d6465f66b7451c6a15584059e5d9c17b
Gianni Manilia <giannimanilia@gmail.com>
2017-02-01 10:53:54 -0400

    Estructura para manejar las recargas

---
+++ Tarea3CI3715/Recarga.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 1, 2017
+
+@author: Julio Fuenmayor 13-10488
+@author: Gianni Manilia 12-10903
+'''
+
+class Recarga(object):
+
+    # El argumento fecha debe ser de tipo date
+    def __init__(self, monto, fecha):
+        self.monto = monto
+        self.fecha = fecha
