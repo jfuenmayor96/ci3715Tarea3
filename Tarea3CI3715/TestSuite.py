d38b6e1b65b6195889a7550da8c61ddb7f57057b
Julio <jfuenmayor96@gmail.com>
2017-01-31 23:04:17 -0400

    Agregados nuevos asserts

--- Tarea3CI3715/TestSuite.py
+++ Tarea3CI3715/TestSuite.py
@@ -13,17 +13,23 @@ from BilleteraElectronica import *
 class Test(unittest.TestCase):
 
 
+    
     def testIdentificadorDeInstancia(self):
         
         billetera = BilleteraElectronica(1310488)
         
         assert(billetera.identificador is not None), "Usted introdujo un identificador nulo."
+        self.assertEqual(billetera.identificador, 1310488, "El identificador de la instancia es incorrecto.")
 
     def testDatosDelUsuario(self):
         
         billetera = BilleteraElectronica(1310488, "julio", "fuenmayor", 24818828)        
 
-        assert(billetera.nombre is not None and billetera.apellido is not None and billetera.ci is not None), "Usted introdujo un dato personal erroneo. "
+        assert(billetera.nombre is not None and billetera.apellido is not None and billetera.ci is not None), "Usted introdujo un dato personal nulo. "
+        self.assertEqual(billetera.nombre, "julio", "El nombre de la instancia es incorrecto.")
+        self.assertEqual(billetera.apellido, "fuenmayor", "El apellido de la instancia es incorrecto.")
+        self.assertEqual(billetera.ci, 24818828, "La cédula de identidad de la instancia es incorrecto.")
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
