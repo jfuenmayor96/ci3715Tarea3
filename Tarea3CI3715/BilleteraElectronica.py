35aa149740676fd11962c8cd3b81030d93cba076
Julio <jfuenmayor96@gmail.com>
2017-01-31 22:02:18 -0400

    Creación inicial de la clase BilleteraElectronica

---
+++ Tarea3CI3715/BilleteraElectronica.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 31, 2017
+
+@author: Julio Fuenmayor 13-10488
+@author: Gianni Manilia 12-10903
+'''
+
+class BilleteraElectronica(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
